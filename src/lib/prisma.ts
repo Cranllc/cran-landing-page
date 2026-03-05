30bac2a246769d0719b7674be7364e604937a15f
Tyler Boyer <93006580+TyBoye@users.noreply.github.com>
2026-03-04 20:40:15 -0600

    fix: explicit Prisma connection pooler to prevent magic link verification failures in Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasourceUrl: process.env.cran_POSTGRES_PRISMA_URL,
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
